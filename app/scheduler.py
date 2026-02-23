6796eec52f4124a8b10f6708c9ccdbaad003c24d
h00emyspi <82257787+h00emyspi@users.noreply.github.com>
2026-02-23 22:18:48 +0300

    sync app/scheduler.py

--- app/scheduler.py
+++ app/scheduler.py
@@ -9,12 +9,12 @@ async def _post_slot(storage: Storage, slot: str):
     await post_scheduled(storage=storage, slot=slot)
 
 
-def setup_scheduler(*, storage: Storage, post_times: list[str]):
-    scheduler = AsyncIOScheduler()
+def setup_scheduler(*, storage: Storage, post_times: list[str], timezone: str = "UTC"):
+    scheduler = AsyncIOScheduler(timezone=timezone)
 
     for t in post_times:
         hh, mm = t.split(":")
-        trigger = CronTrigger(hour=int(hh), minute=int(mm))
+        trigger = CronTrigger(hour=int(hh), minute=int(mm), timezone=timezone)
         scheduler.add_job(_post_slot, trigger=trigger, kwargs={"storage": storage, "slot": t})
 
     return scheduler
